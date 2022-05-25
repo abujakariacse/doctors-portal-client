6df1d9a656f343215c67ed0660bba49659c51105
abujakariacse <abujakaria316@gmail.com>
2022-05-25 07:39:37 +0600

    treatment part added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../../assets/images/fluoride.png';
 import cavity from '../../../assets/images/cavity.png';
 import whitening from '../../../assets/images/whitening.png';
+import treatment from '../../../assets/images/treatment.png';
 import Service from '../Service/Service';
 
 
@@ -19,6 +20,18 @@ const Services = () => {
                 {
                     services.map(service => <Service key={service._id} service={service}></Service>)
                 }
+            </div>
+            <div>
+                <div className="hero lg:w-2/3 mx-auto lg:mt-20">
+                    <div className="hero-content grid grid-cols-1 lg:grid-cols-2 gap-10">
+                        <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" alt='treatment' />
+                        <div>
+                            <h1 className="text-3xl lg:text-4xl font-bold text-accent">Exceptional Dental Care, on Your Terms</h1>
+                            <p className="py-6 text-sm">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                            <button className="btn btn-primary text-white font-bold">Get Started</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div >
     );
